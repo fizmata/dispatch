64f64a7a37d5bf63bc13a8243802384cd00f933c
nizami1337 <nizami@nizamihasanov.com>
2025-05-03 14:23:12 +0300

    test

--- src/dispatch/static/dispatch/src/components/layouts/BasicLayout.vue
+++ src/dispatch/static/dispatch/src/components/layouts/BasicLayout.vue
@@ -6,7 +6,7 @@
       <v-app-bar flat style="border-bottom: 1px solid #d2d2d2 !important" color="background0">
         <template #prepend>
           <router-link :to="{ name: 'IncidentOverview' }" style="text-decoration: none">
-            <span class="button font-weight-bold">D I S P A T C H</span>
+            <span class="button font-weight-bold">E X I R</span>
           </router-link>
         </template>
       </v-app-bar>
